3764120a3fac4458d63ac771c4ee17c952c04761
SukkulaVenukseen <60460800+SukkulaVenukseen@users.noreply.github.com>
2021-04-28 18:04:46 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,55 +6,61 @@ const config = {
 
 this.config = config;
 this.nbrOfBubbles = config.BUBBLE_AMOUNT;
-this.maxSpd = config.BUBBLE_MAX_SPEED;
+this.maxSpeed = config.BUBBLE_MAX_SPEED;
 
-function getNumber() {
-    var id = this.dataset.value;
-    console.log("Bubble number " + id + ".");
+function getValue() {
+    // Function for getting the right number of the clicked bubble
+    var value = this.dataset.value;
+    console.log("Bubble number " + value + ".");
 }
-const buttons = document.querySelectorAll('.bubble-item');
-buttons.forEach(button => button.addEventListener('click', getNumber, false));
+const numbers = document.querySelectorAll('.bubble-item');
+numbers.forEach(button => button.addEventListener('click', getValue, false));
 
-function getRandomSpd() {
-    return Math.floor(Math.random() * maxSpd) + 1;
+function getRandomSpeed() {
+    // Setting random speed for bubbles
+    return Math.floor(Math.random() * maxSpeed) + 1;
 }
 
-let bg = {
+let area = {
+    // Setting up the bubble-area
     element: document.getElementById("bubble-container"),
     width: document.getElementById("bubble-container").offsetWidth,
     height: document.getElementById("bubble-container").offsetHeight
 };
 
 var widthBody = document.getElementById("body").offsetWidth;
-var wSpace = widthBody - bg.width;
+var space = widthBody - bg.width;
 
-let bubble = {   
+let bubble = {  
+    // Bubble configurations
+    
     add: function(dx, dy) {
+        // Setting up properties of the new bubbles
         let newBubble = Object.create(this);
         newBubble.dx = dx;
         newBubble.dy = dy;
         newBubble.radius = 20;
         newBubble.element = document.getElementById("bubble");
-        bg.element.appendChild(newBubble.element);
+        area.element.appendChild(newBubble.element);
         return newBubble;
     },
-
-    initialize: function (x, y) {
+    start: function (x, y) {
+        // base starting point of the bubbles w/o modifiers
         this.element.style.left = x + "px";
         this.element.style.top = y + "px";
     },
-
     collision: function (x, y) {
-        if (x + this.dx > widthBody - wSpace / 2 - this.radius * 2 || x + this.dx - wSpace / 2 + this.radius < this.radius) {
+        // Bouncing of the bubble when they hit the walls of the container
+        if (x + this.dx > widthBody - space / 2 - this.radius * 2 || x + this.dx - space / 2 + this.radius < this.radius) {
             this.dx = -this.dx;
         }
-        if (y + this.dy > (bg.height + 85) - this.radius || y + this.dy + (bg.height - 385) < this.radius) {
+        if (y + this.dy > (area.height + 85) - this.radius || y + this.dy + (area.height - 385) < this.radius) {
             this.dy = -this.dy;
         }
     },
-
     draw: function (x, y) {
-        this.initialize(x, y);
+        // Drawing the bubbles
+        this.start(x, y);
         let bubbleObj = this;
         setTimeout (function () {
             bubbleObj.collision(x, y);
@@ -63,24 +69,24 @@ let bubble = {
     }
 };
 
-let bubble1 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble2 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble3 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble4 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble5 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble6 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble7 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble8 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble9 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
-let bubble10 = bubble.add(getRandomSpd(maxSpd), getRandomSpd(maxSpd));
+let bubble1 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble2 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble3 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble4 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble5 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble6 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble7 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble8 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble9 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
+let bubble10 = bubble.add(getRandomSpeed(maxSpeed), getRandomSpd(maxSpeed));
 
-bubble1.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble2.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble3.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble4.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble5.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble6.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble7.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble8.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble9.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
-bubble10.draw((bg.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble1.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble2.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble3.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble4.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble5.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble6.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble7.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble8.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble9.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
+bubble10.draw((area.width / 2), Math.floor(Math.random() * 2) + 150);
